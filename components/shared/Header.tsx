a1fcbd97e919bcaf5ef11fea4ee243c972406518
Thang Vo <thanhthang3375@gmail.com>
2024-01-13 00:04:26 +0700

    update Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image src="/assets/images/logo.svg" width={128} height={38} alt="FlashPOD"/>
+          <Image src="/assets/images/logo.svg" width={75} height={75} alt="FlashPOD"/>
         </Link>
 
         <SignedIn>
--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -7,6 +7,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import Image from "next/image"
+import { Separator } from "../ui/separator"
 
 const MobileNav = () => {
   return (
@@ -15,14 +16,9 @@ const MobileNav = () => {
         <SheetTrigger className="align-middle">
           <Image src="/assets/icons/menu.svg" width={24} height={24} alt="menu" className="cursor-pointer" />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image src={'/assets/images/logo.svg'} width={50} height={50} alt="logo"/>
+          <Separator className="border border-gray-50"/>
         </SheetContent>
       </Sheet>
     </nav>
